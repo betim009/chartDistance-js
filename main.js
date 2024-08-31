d0e235109c81cb49864fba05b77cc9dfdaa296c1
Alberto <betim009@gmail.com>
2024-08-31 09:29:42 -0300

    mychart

--- main.js
+++ main.js
@@ -1,41 +1,45 @@
-import './style.css'
-
-const data = {
-  labels: ['Hospital', 'Delegacia'],
-  datasets: [
-    {
-      label: 'Distância (km)',
-      data: [4.4, 2.4],
-      backgroundColor: 'rgba(54, 162, 235, 0.5)',
-      borderColor: 'rgba(54, 162, 235, 1)',
-      borderWidth: 1,
-      yAxisID: 'y',
-    },
-    {
-      label: 'Carro (min)',
-      data: [10, 5],
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      borderColor: 'rgba(255, 99, 132, 1)',
-      borderWidth: 1,
-      yAxisID: 'y1',
-    },
-    {
-      label: 'Bicicleta (min)',
-      data: [20, 10],
-      backgroundColor: 'rgba(75, 192, 192, 0.5)',
-      borderColor: 'rgba(75, 192, 192, 1)',
-      borderWidth: 1,
-      yAxisID: 'y1',
-    },
-    {
-      label: 'Caminhada (min)',
-      data: [40, 20],
-      backgroundColor: 'rgba(153, 102, 255, 0.5)',
-      borderColor: 'rgba(153, 102, 255, 1)',
-      borderWidth: 1,
-      yAxisID: 'y1',
-    },
-  ],
-};
-
-
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Hospital', 'Delegacia'],
+    datasets: [
+      {
+        label: 'Carro',
+        data: [10, 5],
+        backgroundColor: 'rgba(75, 192, 192, 0.5)',
+      },
+      {
+        label: 'Bicicleta',
+        data: [20, 15],
+        backgroundColor: 'rgba(54, 162, 235, 0.5)',
+      },
+      {
+        label: 'Caminhada',
+        data: [50, 40],
+        backgroundColor: 'rgba(255, 206, 86, 0.5)',
+      }
+    ]
+  },
+  options: {
+    responsive: true,
+    indexAxis: 'y', // Transforma em barras horizontais
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Tempo (min)',
+        },
+        ticks: {
+          beginAtZero: true
+        }
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Localidade',
+        }
+      }
+    }
+  }
+});
