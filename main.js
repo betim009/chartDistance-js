37b0fba2da5d438337ffbc7393779fb8bca51ee2
Alberto <betim009@gmail.com>
2024-08-31 08:43:25 -0300

    update: data

--- main.js
+++ main.js
@@ -1,4 +1,41 @@
 import './style.css'
 
+const data = {
+  labels: ['Hospital', 'Delegacia'],
+  datasets: [
+    {
+      label: 'Distância (km)',
+      data: [4.4, 2.4],
+      backgroundColor: 'rgba(54, 162, 235, 0.5)',
+      borderColor: 'rgba(54, 162, 235, 1)',
+      borderWidth: 1,
+      yAxisID: 'y',
+    },
+    {
+      label: 'Carro (min)',
+      data: [10, 5],
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 1,
+      yAxisID: 'y1',
+    },
+    {
+      label: 'Bicicleta (min)',
+      data: [20, 10],
+      backgroundColor: 'rgba(75, 192, 192, 0.5)',
+      borderColor: 'rgba(75, 192, 192, 1)',
+      borderWidth: 1,
+      yAxisID: 'y1',
+    },
+    {
+      label: 'Caminhada (min)',
+      data: [40, 20],
+      backgroundColor: 'rgba(153, 102, 255, 0.5)',
+      borderColor: 'rgba(153, 102, 255, 1)',
+      borderWidth: 1,
+      yAxisID: 'y1',
+    },
+  ],
+};
 
 
